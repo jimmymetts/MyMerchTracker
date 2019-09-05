91250844f9b583fa717a81b4354f8595556d86a7
Jimmy <jimmymetts@bellsouth.net>
2019-09-05 18:43:12 -0500

    deleted merch views

---
+++ Models/MerchType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMerchTracker.Models
+{
+    public class MerchType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string label { get; set; }
+
+
+
+        public virtual ICollection<Merch> Merchs { get; set; }
+    }
+
+}
+
